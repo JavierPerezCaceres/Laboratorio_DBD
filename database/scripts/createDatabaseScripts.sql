90413032e88a978191baa1d443e57bb4a1a14939
Unknown <jorge.ayala@usach.cl>
2019-06-03 19:23:35 -0400

    Feat: Scripts SQL pt 5

--- database/scripts/createDatabaseScripts.sql
+++ database/scripts/createDatabaseScripts.sql
@@ -6,6 +6,13 @@ DROP TABLE IF EXISTS clients CASCADE;
 DROP TABLE IF EXISTS payment_methods CASCADE;
 DROP TABLE IF EXISTS card_payments CASCADE;
 DROP TABLE IF EXISTS purchase_orders CASCADE;
+DROP TABLE IF EXISTS menu_reservations CASCADE;
+DROP TABLE IF EXISTS menus CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS ingredients CASCADE;
+DROP TABLE IF EXISTS menu_products CASCADE;
+DROP TABLE IF EXISTS product_ingredients CASCADE;
+
 
 
 --=====================================================================
@@ -36,6 +43,47 @@ CREATE TABLE IF NOT EXISTS card_payments (
  ;
 -- -------------------------------------------------------------
 
+-- CREATE TABLE "ingredients" --------------------------------------
+CREATE TABLE IF NOT EXISTS ingredients ( 
+	"id" serial NOT NULL,
+	"created_at" Timestamp( 0 ) Without Time Zone,
+	"updated_at" Timestamp( 0 ) Without Time Zone,
+	"name" Character Varying( 45 ) NOT NULL
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE TABLE "menus" --------------------------------
+CREATE TABLE IF NOT EXISTS menus ( 
+	"created_at" Timestamp( 0 ) Without Time Zone,
+	"updated_at" Timestamp( 0 ) Without Time Zone,
+	"name" Character Varying( 45 ) NOT NULL,
+	"total_price" Integer NOT NULL,
+	"discount" Integer NOT NULL,
+	"restaurant_id" Integer NOT NULL
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE TABLE "menu_products" --------------------------------
+CREATE TABLE IF NOT EXISTS menu_products ( 
+	"created_at" Timestamp( 0 ) Without Time Zone,
+	"updated_at" Timestamp( 0 ) Without Time Zone,
+	"price" Integer NOT NULL,
+	"menu_id" Integer NOT NULL,
+	"product_id" Integer NOT NULL
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE TABLE "menu_reservations" --------------------------------
+CREATE TABLE IF NOT EXISTS menu_reservations ( 
+	"created_at" Timestamp( 0 ) Without Time Zone,
+	"updated_at" Timestamp( 0 ) Without Time Zone,
+	"price" Integer NOT NULL,
+	"quantity" Integer NOT NULL,
+	"menu_id" Integer NOT NULL,
+	"purchase_order_id" Integer NOT NULL
+ ;
+-- -------------------------------------------------------------
+
 -- CREATE TABLE "payment_methods" ------------------------------
 CREATE TABLE IF NOT EXISTS payment_methods ( 
 	"id" serial NOT NULL,
@@ -46,6 +94,23 @@ CREATE TABLE IF NOT EXISTS payment_methods (
  ;
 -- -------------------------------------------------------------
 
+-- CREATE TABLE "products" --------------------------------------
+CREATE TABLE IF NOT EXISTS products ( 
+	"id" serial NOT NULL,
+	"created_at" Timestamp( 0 ) Without Time Zone,
+	"updated_at" Timestamp( 0 ) Without Time Zone,
+	"name" Character Varying( 45 ) NOT NULL
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE TABLE "product_ingredients" --------------------------------
+CREATE TABLE IF NOT EXISTS product_ingredients ( 
+	"created_at" Timestamp( 0 ) Without Time Zone,
+	"updated_at" Timestamp( 0 ) Without Time Zone,
+	"product_id" Integer NOT NULL,
+	"ingredient_id" Integer NOT NULL
+ ;
+-- -------------------------------------------------------------
 
 -- CREATE TABLE "purchase_orders" ------------------------------
 CREATE TABLE IF NOT EXISTS purchase_orders ( 
@@ -77,9 +142,33 @@ ALTER TABLE card_payments
     ADD CONSTRAINT card_payments_pkey
     PRIMARY KEY (id);
 
+ALTER TABLE ingredients
+ADD CONSTRAINT ingredients_pkey
+PRIMARY KEY (id);
+
+ALTER TABLE menus
+    ADD CONSTRAINT menus_pkey
+    PRIMARY KEY (id);
+
+ALTER TABLE menu_products
+    ADD CONSTRAINT menu_products_pkey
+    PRIMARY KEY (id);
+
+ALTER TABLE menu_reservations
+    ADD CONSTRAINT menu_reservations_pkey
+    PRIMARY KEY (id);
+
 ALTER TABLE payment_methods
     ADD CONSTRAINT payment_methods_pkey
     PRIMARY KEY (id);
+
+ALTER TABLE products
+ADD CONSTRAINT products_pkey
+PRIMARY KEY (id);
+
+ALTER TABLE product_ingredients
+ADD CONSTRAINT product_ingredients_pkey
+PRIMARY KEY (id);
 
 ALTER TABLE purchase_orders
     ADD CONSTRAINT purchase_orders_pkey
@@ -102,6 +191,55 @@ ALTER TABLE card_payments
 --===========================
 -- FOREIGN KEYS
 --===========================
+
+-- CREATE "menus_restaurant_id_foreign" -----
+--ALTER TABLE menus
+--	ADD CONSTRAINT menus_restaurant_id_foreign
+--	FOREIGN KEY ( restaurant_id )
+--	REFERENCES restaurants ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
+
+-- CREATE "menu_products_menu_id_foreign" -----
+ALTER TABLE menu_products
+	ADD CONSTRAINT menu_products_menu_id_foreign
+	FOREIGN KEY ( menu_id )
+	REFERENCES menus ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
+
+-- CREATE "menu_products_product_id_foreign" -----
+ALTER TABLE menu_products
+	ADD CONSTRAINT menu_products_product_id_foreign
+	FOREIGN KEY ( product_id )
+	REFERENCES products ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
+
+-- CREATE "menu_reservations_menu_id_foreign" -----
+ALTER TABLE menu_reservations
+	ADD CONSTRAINT menu_reservations_menu_id_foreign
+	FOREIGN KEY ( menu_id )
+	REFERENCES menus ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
+
+-- CREATE "menu_reservations_purchase_order_id_foreign" -----
+ALTER TABLE menu_reservations
+	ADD CONSTRAINT menu_reservations_purchase_order_id_foreign
+	FOREIGN KEY ( purchase_order_id )
+	REFERENCES purchase_orders ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
+
+-- CREATE "product_ingredients_product_id_foreign" -----
+ALTER TABLE product_ingredients
+	ADD CONSTRAINT product_ingredients_product_id_foreign
+	FOREIGN KEY ( product_id )
+	REFERENCES products ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
+
+-- CREATE "product_ingredients_ingredient_id_foreign" -----
+ALTER TABLE product_ingredients
+	ADD CONSTRAINT product_ingredients_ingredient_id_foreign
+	FOREIGN KEY ( ingredient_id )
+	REFERENCES ingredients ( "id" ) MATCH SIMPLE;
+-- -------------------------------------------------------------
 
 -- CREATE "purchase_orders_payment_method_id_foreign" -----
 ALTER TABLE purchase_orders
