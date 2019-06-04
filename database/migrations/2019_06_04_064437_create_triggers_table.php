fa63788a03b67f1bd2af4bd73ec1dd01156256cd
Nicole Martín Ramos <nicole.martin@usach.cl>
2019-06-04 03:25:10 -0400

    Fix: triggers

--- database/migrations/2019_06_04_064437_create_triggers_table.php
+++ database/migrations/2019_06_04_064437_create_triggers_table.php
@@ -14,28 +14,26 @@ class CreateTriggersTable extends Migration
     public function up()
     {
         DB::statement('
-            CREATE OR REPLACE FUNCTION llenarAsientos()
+        CREATE OR REPLACE FUNCTION available_table()
             RETURNS trigger AS
             $$
                 DECLARE
-                i INTEGER := NEW.cantidad_asientos;
+                i INTEGER := NEW.people_quantity;
                 j INTEGER := 0;
                 valor INTEGER := NEW.id;
                 BEGIN           
                 LOOP 
                     EXIT WHEN j = i;
                     j := j + 1;
-                    INSERT INTO asientos( vuelo_id, numero_asiento, disponibilidad,tipo_asiento,created_at) VALUES 
-                    (valor, j, true, 1, NEW.created_at );
+                    INSERT INTO tables (capacity,number,avaible,restaurant_id,created_at,updated_at) 
+                    VALUES 
+                    (10, j, 0, valor, NEW.created_at, NEW.updated_at );
                 END LOOP ;
                 RETURN NEW;
             END
             $$ LANGUAGE plpgsql;
         ');
-        DB::unprepared('
-        CREATE TRIGGER asiento_Vuelo AFTER INSERT ON vuelos FOR EACH ROW
-        EXECUTE PROCEDURE llenarAsientos();
-        ');
+
     }
 
     /**
