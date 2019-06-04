2134ab5acf3c5cabaa67a76cdeafb714089d7805
Nicole Martín Ramos <nicole.martin@usach.cl>
2019-06-04 03:41:15 -0400

    Fix: Scripts ok

--- database/scripts/insertDatabaseScripts.sql
+++ database/scripts/insertDatabaseScripts.sql
@@ -1,3 +1,4 @@
+  
 --=======================
 --	INSERT
 --=======================
@@ -24,7 +25,7 @@ VALUES ( '2019-06-02 05:56:48','1','martin','nicole','+23456789',
 
 INSERT INTO "public"."users" ("id", "email", "password", "role_id", "client_id", 
 	"name", "email_verified_at", "remember_token", "created_at", "updated_at")
-VALUES ( '1', 'nicole.martin@usach.cl', 'passwordXD', '3', '1');
+VALUES ( '1', 'nicole.martin@usach.cl', 'passwordXD', '3', '1','Nicole','2019-06-03 05:56:48','456543234567765','2019-06-03 05:56:48','2019-06-03 05:56:48');
 
 -- Restaurantes
 
@@ -32,7 +33,7 @@ INSERT INTO "public"."restaurants" ( "id","created_at", "updated_at",
 	"category", "contact_number", "kitchen_type", "opening_hour", "closing_hour",
 	"person_cost", "wait_time", "direction", "user_id")
 VALUES ( '1','2019-06-03 05:56:48','2019-06-03 05:56:48','China','+56923423422','Vegetariana',
-	'08:00:00','20:00:00','9000','45','Tres Norte',"1");
+	'08:00:00','20:00:00','9000','45','Tres Norte','1');
 
 -- Menus
 
@@ -66,25 +67,25 @@ VALUES ( '2019-06-02 05:56:48','1','1590','1','1',
 -- Deliveries
 
 INSERT INTO "public"."deliveries" ( "id","created_at", "updated_at",
-	"receptor_name","contact_number","extra_wait_time","delivery_address","restaurant_id",)
+	"receptor_name","contact_number","extra_wait_time","delivery_address","restaurant_id")
 VALUES ( '1','2019-06-03 05:56:48','2019-06-03 05:56:48','Cristian Matias Rodriguez Salazar',
 '+23356679','30','Simon Bolivar 345','1');
 
 -- Historial de página
 
-INSERT INTO "public"."webpage_records" ( "id","created_at", "updated_at", "action",)
+INSERT INTO "public"."webpage_records" ( "id","created_at", "updated_at", "action")
 VALUES ( '1','2019-06-03 05:56:48','2019-06-03 05:56:48','Se ha registrado el usuario id:1 en la pagina');
 
 -- Valoraciones
 
 INSERT INTO "public"."valorations" ( "id","created_at", "updated_at",
-"score","comment","client_id","restaurant_id",)
+"score","comment","client_id","restaurant_id")
 VALUES ( '1','2019-06-03 05:56:48','2019-06-03 05:56:48','4','Buen servicio, pero pudo ser mejor.','1','1');
 
 -- Solicitud de restaurants
 
 INSERT INTO "public"."restaurant_requests" ( "id","created_at", "updated_at",
-"company_rut","cod_sis","owner_name","condition","user_id",)
+"company_rut","cod_sis","owner_name","condition","user_id")
 VALUES ( '1','2019-06-03 05:56:48','2019-06-03 05:56:48','123456789','746539887','Carlos Fernandes Serra Herrero','1','1');
 
 -- Pago con Tarjeta
@@ -118,7 +119,7 @@ VALUES ( '2019-06-02 05:56:48','1','5790','1','1','1', '2019-06-03 05:56:48');
 -- Categoria
 
 INSERT INTO "public"."categories" ( "id","created_at", "updated_at",
-	"name",)
+	"name")
 VALUES ( '1','2019-06-03 05:56:48','2019-06-03 05:56:48','Sin Gluten');
 
 -- Producto Categoria
@@ -155,12 +156,6 @@ VALUES ('1', '2019-06-03 05:56:48', '2019-06-03 05:56:48', 'Pudahuel', '1');
 
 INSERT INTO "public"."addresses" ("id","created_at", "updated_at", "street", "number", "district_id", "client_id")
 VALUES ('1', '2019-06-03 05:56:48', '2019-06-03 05:56:48', 'Av. Serrano', '528-B', '1', '1');
-
-
-
-
-
-
 
 
 
